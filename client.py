0bb674ea40ebb2da75a04be87a473dd0c8f67154
BeyLung_Dev <132239298+BeylungDev@users.noreply.github.com>
2024-09-01 21:51:31 -0300

    Add files via upload

--- client.py
+++ client.py
@@ -4,21 +4,25 @@ import zlib
 import time
 import os
 
+# Função não utilizada, era para ficar limpando o terminal mas decidi não utilizar no final
 def clear_console():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+# Calcula o cheksum
 def checksum(data):
     return zlib.crc32(data.encode())
 
 def main():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_address = ('localhost', 12345)
+    server_address = ('127.0.0.1', 3333)
     client_socket.settimeout(2)  # Define o timeout para 2 segundos
     
+    ack_number = 0  # ACK inicial
+
     while True:
         # Escolha dos modos
         print("|------------------------------------------------------------------------------------------|\n")
-        client_choice = input("Cliente: Escolha o modo para ENVIAR suas mensagens:\n1 - Normal\n2 - Corromper\n3 - Perder\nEscolha: ")
+        client_choice = input("Escolha o modo para ENVIAR suas mensagens:\n1 - Normal\n2 - Corromper\n3 - Perder\nEscolha: ")
         if client_choice == '1':
             client_mode = "normal"
         elif client_choice == '2':
@@ -29,7 +33,7 @@ def main():
             print("Escolha inválida. Tente novamente.")
             continue
 
-        server_choice = input("\nCliente: Escolha o modo para RECEBER as mensagens do servidor:\n1 - Normal\n2 - Corromper\n3 - Perder\nEscolha: ")
+        server_choice = input("\nEscolha o modo para RECEBER as mensagens do servidor:\n1 - Normal\n2 - Corromper\n3 - Perder\nEscolha: ")
         if server_choice == '1':
             server_mode = "normal"
         elif server_choice == '2':
@@ -43,59 +47,66 @@ def main():
         # Envia os modos para o servidor
         modes = f"{client_mode},{server_mode}"
         client_socket.sendto(modes.encode(), server_address)
-        print(f"\nCliente: Modos escolhidos: ENVIAR - '{client_mode}', RECEBER - '{server_mode}'.")
+        print(f"\nModos escolhidos: ENVIAR - '{client_mode}', RECEBER - '{server_mode}'.")
 
         try:
             # Recebe confirmação do servidor
             ack, _ = client_socket.recvfrom(1024)
             if ack.decode() == "ACK":
-                print("Cliente: Sincronizado com o servidor. Pronto para enviar dados...\n")
+                print("Sincronizado com o servidor. Pronto para enviar dados...\n")
             else:
                 print("Erro na sincronização com o servidor.")
                 continue
         
         except socket.timeout:
-            print("Cliente: Tempo de espera excedido. Tentando novamente...\n")
+            print("Tempo de espera excedido. Tentando novamente...\n")
             continue
 
         # Captura a posição do mouse
         x, y = pyautogui.position()
         data = f'{x},{y}'
         data_checksum = checksum(data)
-        packet = f'{data}, {data_checksum}'
+        packet = f'{data}, {data_checksum}, {ack_number}'
         
         if client_mode == 'corromper':  # Corromper
             data_checksum += 1  # Corrompe o checksum
-            packet = f'{data}, {data_checksum}'
-            print("Cliente: Pacote será corrompido antes do envio.")
+            packet = f'{data}, {data_checksum}, {ack_number}'
+            print("Pacote será corrompido antes do envio.")
         elif client_mode == 'perder':  # Perder
-            print("Cliente: Simulando perda de pacote...")
+            print("Simulando perda de pacote...")
             time.sleep(2)  # Simula o tempo de perda
-            print("Cliente: Tempo de espera excedido. Pacote considerado como perdido.\n")
-            #continue  # Retorna ao início do loop para nova escolha
+            print("Tempo de espera excedido. Pacote considerado como perdido.\n")
+            continue  # Retorna ao início do loop para nova escolha
 
         # Envia o pacote para o servidor
         client_socket.sendto(packet.encode(), server_address)
-        print(f"Cliente: Pacote enviado: {packet}")
+        print(f"Pacote enviado: {packet}")
         
         try:
             # Recebe a resposta do servidor
             response, _ = client_socket.recvfrom(1024)
             response = response.decode()
-            print(f"Cliente: Resposta do servidor recebida.")
+            if client_mode != 'perder':
+                print(f"Resposta do servidor recebida.")
             
-            # Verifica o checksum da resposta
-            response_data, response_checksum = response.rsplit(',', 1)
+            # Verifica o checksum da resposta e o ACK
+            response_data, response_checksum, received_ack = response.rsplit(',', 2)
             response_checksum = int(response_checksum)
+            received_ack = int(received_ack)
             calculated_checksum = checksum(response_data)
             
             if calculated_checksum == response_checksum:
-                print(f"Cliente: Resposta recebida corretamente: {response}\n")
+                if received_ack == ack_number:
+                    print(f"Resposta recebida corretamente com ACK {received_ack}: {response_data} / Checksum da resposta: {response_checksum}, Checksum esperado da resposta: {calculated_checksum}.\n")
+                    ack_number = 1 - ack_number  # Alterna o ACK para a próxima mensagem
+                else:
+                    print("ACK incorreto.\n")
             else:
-                print("Cliente: Resposta corrompida.\n")
+                print(f"Resposta corrompida / Checksum da resposta: {response_checksum}, Checksum esperado da resposta: {calculated_checksum}.\n")
         
         except socket.timeout:
-            print("Cliente: Tempo de espera excedido. Resposta não recebida.\n")
+            ack_number = 1 - ack_number
+            print("Tempo de espera excedido. Resposta não recebida.\n")
 
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -2,74 +2,81 @@ import socket
 import zlib
 import time
 
+# Função para calcular o checksum:
 def checksum(data):
     return zlib.crc32(data.encode())
 
 def main():
+    # Conecta-se ao cliente
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_socket.bind(('localhost', 12345))
+    server_socket.bind(('0.0.0.0', 3333)) 
+    server_socket.settimeout(2)
     
-    print("Servidor: Aguardando as escolhas do cliente...")
+    print("Aguardando as escolhas do cliente...")
 
     while True:
         try:
-            print("|------------------------------------------------------------------------------------------|\n")
             # Recebe os modos do cliente
             modes, client_address = server_socket.recvfrom(1024)
             client_mode, server_mode = modes.decode().split(',')
-            print(f"Servidor: Modos recebidos - ENVIAR (cliente): '{client_mode}', RECEBER (servidor): '{server_mode}'.")
+            print("|------------------------------------------------------------------------------------------|\n")
+            print(f"Modos recebidos - ENVIAR (cliente): '{client_mode}', RECEBER (servidor): '{server_mode}'.")
             
             # Envia confirmação ao cliente
             server_socket.sendto("ACK".encode(), client_address)
-            print("Servidor: Sincronizado com o cliente. Pronto para receber dados...\n")
+            print("Sincronizado com o cliente. Pronto para receber dados...\n")
         
             while True:
                 try:
                     # Recebe o pacote do cliente
                     packet, client_address = server_socket.recvfrom(1024)
                     packet = packet.decode()
-                    print(f"Servidor: Pacote recebido: {packet}")
+                    if client_mode != 'perder':
+                        print(f"Pacote recebido: {packet}")
 
                     # Tenta separar o pacote e calcular o checksum
                     try:
-                        data, received_checksum_str = packet.rsplit(',', 1)
+                        data, received_checksum_str, received_ack = packet.rsplit(',', 2)
                         received_checksum = int(received_checksum_str.strip())
+                        received_ack = int(received_ack.strip())
                         calculated_checksum = checksum(data)
                     except ValueError as e:
-                        print(f"Servidor: Erro ao processar o pacote - {e}")
+                        print(f"Erro ao processar o pacote - {e}")
                         break
 
                     # Verifica se o checksum recebido coincide com o calculado
                     if calculated_checksum == received_checksum:
-                        if client_mode == 'perder':
-                            response_data = "Pacote perdido."
-                        else:
-                            response_data = "Pacote recebido corretamente"
+                        print(f"ACK {received_ack} recebido corretamente.")
+                        response_data = f"ACK {received_ack}. Pacote recebido corretamente, Checksum recebido: {received_checksum}, Checksum esperado: {calculated_checksum}"
+                    # Caso o checksum seja diferente, significa que está corrompido e então envia uma resposta ao cliente
                     else:
-                        response_data = "Pacote corrompido"
+                        print(f"Checksum incorreto. Esperado: {calculated_checksum}. Recebido: {received_checksum}.")
+                        response_data = f'Pacote corrompido, Checksum recebido: {received_checksum}, Checksum esperado: {calculated_checksum}'
                     
+                    # Calcula o checksum da resposta
                     response_checksum = checksum(response_data)
 
-                    if server_mode == 'corromper':  # Corromper
-                        response_checksum += 1  # Corrompe o checksum
-                        print("Servidor: Resposta será corrompida antes do envio.")
-                    elif server_mode == 'perder':  # Perder
-                        print("Servidor: Simulando perda de resposta...")
-                        time.sleep(2)  # Simula o tempo de perda
-                        print("Servidor: Tempo de resposta excedido. Resposta não enviada.\n")
-                        break  # Volta ao início para nova espera
+                    if server_mode == 'corromper':  # Corromper o envio do servidor para o cliente
+                        response_checksum += 1  # Corrompe o checksum adicionando +1 ao seu valor
+                        print("Resposta será corrompida antes do envio.")
+                    elif server_mode == 'perder':  # Perder os dados
+                        print("Simulando perda de resposta...")
+                        time.sleep(2)
+                        print("Tempo de resposta excedido. Resposta não enviada.\n")
+                        break
 
-                    response_packet = f'{response_data},{response_checksum}'
+                    response_packet = f'{response_data},{response_checksum},{received_ack}'  # Envia de volta o ACK recebido
                     server_socket.sendto(response_packet.encode(), client_address)
-                    print(f"Servidor: Resposta enviada: {response_packet}\n")
+                    if client_mode != 'perder':
+                        print(f"Resposta enviada com ACK {received_ack}: {response_packet}\n")
                     break
 
                 except socket.timeout:
-                    print("Servidor: Tempo de espera excedido. Nenhum pacote recebido.\n")
+                    print("Tempo de espera excedido. Nenhum pacote recebido.\n")
                     break
         
         except Exception as e:
-            print(f"Servidor: Erro inesperado: {e}\n")
+            continue
 
 if __name__ == '__main__':
     main()
